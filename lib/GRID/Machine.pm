6595b413f4cd13806a3fb48753317a1fb534e04b
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2011-05-18 05:54:32 +0000

    Import of CASIANO/GRID-Machine-0.117 from CPAN.
    gitpan-cpan-distribution: GRID-Machine
    gitpan-cpan-version:      0.117
    gitpan-cpan-path:         CASIANO/GRID-Machine-0.117.tar.gz
    gitpan-cpan-author:       CASIANO
    gitpan-cpan-maturity:     released

--- lib/GRID/Machine.pm
+++ lib/GRID/Machine.pm
@@ -29,7 +29,7 @@ use GRID::Machine::MakeAccessors; # Order is important. This must be the first!
 use GRID::Machine::Message;
 use GRID::Machine::Result;
 
-our $VERSION = "0.116";
+our $VERSION = "0.117";
 
 my %_taken_id;
 {
